030773a03a66227f10efa7d945c7c8aab1b02e37
khushboo kumari <ksingh200601@gmail.com>
2024-12-23 14:55:40 +0530

    payment-model updated

--- model/payment.model.js
+++ model/payment.model.js
@@ -3,8 +3,65 @@ const mongoose = require('mongoose');
 
 
 const petSchema = new mongoose.Schema({
-    paymentsOptions:{
-        type:Array,
+    productId:{
+        type:mongoose.SchemaTypes.ObjectId,
+        ref:'product',
+        required:true,
     },
+    userId:{
+        type:mongoose.SchemaTypes.ObjectId,
+        ref:'user',
+        required:true,
+    },
+    addressId:{
+        type:mongoose.SchemaTypes.ObjectId,
+        ref:'address',
+        required:true,
+    },
+    paymentStatus:{
+        type:String,
+        enum:['pending','Success','Failure'],
+        default:()=>{
+            return 'pending'
+        }
+    },
+    transitionId:{
+        type:String,
+
+    },
+    productName:{
+        type:String,
+        required:true,
+    },
+    producTDescription:{
+        type:String,
+        required:true,
+    },
+    productImage:{
+        type:String,
+        
+    },
+    Amount:{
+        type:String,
+       
+    },
+    quantity:{
+        type:String,
+       
+    },
+    createdAt: {
+        type: Date,
+        default: () => {
+          return Date.now();
+        },
+        immutable: true,
+    },
+    updateAt: {
+        type: Date,
+        default: () => {
+          return Date.now();
+        },
+    }
+
 })
 module.exports = mongoose.model("PAYMENT-GETWAYS", petSchema);
